d80c7337b1c9132a3ac6b439918db5432d28d1f0
Samandar000 <samandarnfh.2020@gmail.com>
2023-04-04 10:57:04 +0500

    post

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import hi
+from .views import hi,addition,subtract,multiply,devide
 
 urlpatterns = [
     path('hi', hi),
+    path('add',addition),
+    path('subtract',subtract),
+    path('multiply',multiply),
+    path('devide',devide)
 ]
--- api/views.py
+++ api/views.py
@@ -3,17 +3,79 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 
 
-@api_view(['GET'])
+
+@api_view(['GET','POST'])
 def hi(request: Request) -> Response:
     '''hi view'''
-    params = request.query_params
-    
-    name = params.get('name', '')
+    if request.method == 'GET':
+        params = request.query_params
+        
+        name = params.get('name', '')
+        print(type(name))
 
-    return Response(f'hi, {name}')
+        return Response(f'hi, {name}')
+    elif request.method == 'POST':
+        params = request.data
+        name = params.get('name','')
 
-
-@api_view
+@api_view(['GET'])
 def addition(request: Request) -> Response:
     '''add two number'''
-    pass
+    if request.method == 'GET':
+        params = request.query_params
+
+        a = params.get('a',0)
+        b = params.get('b',0)
+        c = int(a)+int(b)
+
+        return Response({'answer':c})
+    elif request.method  == 'POST':
+        a = params.get('a',0)
+        b = params.get('b',0)
+        c = int(a)+int(b)
+
+    
+
+@api_view(['GET'])
+def subtract(request: Request) -> Response:
+    '''add two number'''
+    params = request.query_params
+
+    a = params.get('a',0)
+    b = params.get('b',0)
+    c = int(a)-int(b)
+
+    return Response({'answer':c})
+
+
+
+
+@api_view(['GET'])
+def multiply(request: Request) -> Response:
+    '''add two number'''
+    params = request.query_params
+
+    a = params.get('a',0)
+    b = params.get('b',0)
+    c = int(a)*int(b)
+
+    return Response({'answer':c})
+
+
+
+@api_view(['GET'])
+def devide(request: Request) -> Response:
+    '''add two number'''
+    params = request.query_params
+
+    a = params.get('a',0)
+    b = params.get('b',0)
+    b = int(b)
+    
+    
+    if b != 0:
+        c = int(a)/int(b)
+    else:
+        c = int(a)
+
+    return Response({'answer':c})

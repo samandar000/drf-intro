2322e6926051e4af2975417339cc1fefd1f17aa2
djumanov <diyorbekjumanov1120@gmail.com>
2023-04-04 09:41:08 +0500

    rest view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def hi(request: Request) -> Response:
+    '''hi view'''
+    print(request.query_params)
+    print('\n'*5)

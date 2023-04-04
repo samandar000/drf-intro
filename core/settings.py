d049bc0f0b00c1146a998d1493b26b64498a23c3
djumanov <diyorbekjumanov1120@gmail.com>
2023-04-04 09:31:43 +0500

    rest framewrok settings

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # our apps
+    'api',
+
+    # restframework
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
